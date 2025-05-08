22f39b3eaed938d01c5f384500ad302ee0814a52
learing git <87185729+kalaikris@users.noreply.github.com>
2025-05-08 20:06:39 +0530

    Update docker.sh

--- terrafrom/docker.sh
+++ terrafrom/docker.sh
@@ -11,3 +11,4 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
+docker run -d -p 80:80 kalaikris/sample:latest
